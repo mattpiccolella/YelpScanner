d8c0fd1efbc0db668e51776cd47f99cc8529097f
mjp2220 <mjp2220@columbia.edu>
2015-03-26 17:54:47 -0400

    Adding some basic list stuff

--- index.ios.js
+++ index.ios.js
@@ -7,42 +7,108 @@
 var React = require('react-native');
 var {
   AppRegistry,
+  Image,
+  ListView,
   StyleSheet,
   Text,
   View,
 } = React;
 
+var ds = new ListView.DataSource({
+  rowHasChanged: (r1, r2) => r1 !== r2,
+  sectionHeaderHasChanged: (h1, h2) => h1 !== h2,
+});
+
 var YelpScanner = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: ds.cloneWithRows([{"name" : "row1"}, {"name" : "row2"}, {"name" : "row3"}]),
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    this.setState({
+      dataSource: this.state.dataSource,
+      loaded: true,
+    });
+    // We're gonna change this later.
+    /*
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(["firstOne", "secondOne", "thirdOne"],
+          loaded: true,
+        });
+      })
+      .done();
+    */
+  },
+
   render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderItem}
+        style={styles.listView}
+      />
+    );
+  },
+
+  renderLoadingView: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js{'\n'}
-          Press Cmd+R to reload
+        <Text>
+          Loading results...
         </Text>
       </View>
     );
-  }
+  },
+
+  renderItem: function(row) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>{row.name}</Text>
+      </View>
+    );
+  },
 });
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  rightContainer: {
+    flex: 1,
   },
-  instructions: {
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    color: '#333333',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
   },
 });
 
