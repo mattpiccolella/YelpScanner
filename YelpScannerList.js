fb5cc9054fbbfca1d862491164aa4d993f61b888
mjp2220 <mjp2220@columbia.edu>
2015-03-26 18:46:21 -0400

    Starting of our movie app

---
+++ YelpScannerList.js
@@ -0,0 +1,173 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  ListView,
+  PixelRatio,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} = React;
+
+var ds = new ListView.DataSource({
+  rowHasChanged: (r1, r2) => r1 !== r2,
+  sectionHeaderHasChanged: (h1, h2) => h1 !== h2,
+});
+
+var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+var PAGE_SIZE = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+var REQUEST_URL = API_URL + PARAMS;
+
+class YelpScannerList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: ds.cloneWithRowsAndSections(
+        {"section1" : {"r1" : {"name" : "row1"}, "r2" : {"name" : "row2"}}}
+      ),
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRowsAndSections({"movies":responseData.movies}),
+          loaded: true,
+        });
+      })
+      .done();
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <View style={styles.listContainer}>
+        <View style={styles.searchRow}>
+          <TextInput
+            autoCapitalize="none"
+            autoCorrect={false}
+            clearButtonMode="always"
+            onChangeText={this._search.bind(this)}
+            placeholder="Search..."
+            style={styles.searchTextInput}
+          />
+        </View>
+        <ListView
+          style={styles.list}
+          dataSource={this.state.dataSource}
+          renderRow={this._renderRow}
+          renderSectionHeader={this._renderSectionHeader}
+          automaticallyAdjustContentInsets={false}
+        />
+      </View>
+    );
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.listContainer}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  }
+
+  _renderSectionHeader(data, section) {
+    return (
+      <View style={styles.sectionHeader}>
+        <Text style={styles.sectionHeaderTitle}>
+          {section.toUpperCase()}
+        </Text>
+      </View>
+    );
+  }
+
+  _renderRow(row, i) {
+    return (
+      <View>
+        <View style={styles.row}>
+          <Text style={styles.rowTitleText}>{row.title}</Text>
+          <Text style={styles.rowDetailText}>{row.year}</Text>
+        </View>
+        <View style={styles.separator} />
+      </View>
+    );
+  }
+
+  _search(text) {
+    // TODO: Fill this.
+  }
+}
+
+var styles = StyleSheet.create({
+  listContainer: {
+    flex: 1,
+  },
+  list: {
+    backgroundColor: '#eeeeee',
+  },
+  sectionHeader: {
+    padding: 5,
+  },
+  group: {
+    backgroundColor: 'white',
+  },
+  sectionHeaderTitle: {
+    fontWeight: '500',
+    fontSize: 11,
+  },
+  row: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    paddingHorizontal: 15,
+    paddingVertical: 8,
+  },
+  separator: {
+    height: 1 / PixelRatio.get(),
+    backgroundColor: '#bbbbbb',
+    marginLeft: 15,
+  },
+  rowTitleText: {
+    fontSize: 17,
+    fontWeight: '500',
+  },
+  rowDetailText: {
+    fontSize: 15,
+    color: '#888888',
+    lineHeight: 20,
+  },
+  searchRow: {
+    backgroundColor: '#eeeeee',
+    paddingTop: 75,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+  },
+  searchTextInput: {
+    backgroundColor: 'white',
+    borderColor: '#cccccc',
+    borderRadius: 3,
+    borderWidth: 1,
+    height: 30,
+    paddingLeft: 8,
+  },
+});
+
+module.exports = YelpScannerList;
--- index.ios.js
+++ index.ios.js
@@ -5,80 +5,26 @@
 'use strict';
 
 var React = require('react-native');
+var YelpScannerList = require('./YelpScannerList');
 var {
   AppRegistry,
-  Image,
-  ListView,
+  NavigatorIOS,
   StyleSheet,
-  Text,
-  View,
 } = React;
 
-var ds = new ListView.DataSource({
-  rowHasChanged: (r1, r2) => r1 !== r2,
-  sectionHeaderHasChanged: (h1, h2) => h1 !== h2,
-});
-
 var YelpScanner = React.createClass({
-  getInitialState: function() {
-    return {
-      dataSource: ds.cloneWithRows([{"name" : "row1"}, {"name" : "row2"}, {"name" : "row3"}]),
-      loaded: false,
-    };
-  },
-
-  componentDidMount: function() {
-    this.fetchData();
-  },
-
-  fetchData: function() {
-    this.setState({
-      dataSource: this.state.dataSource,
-      loaded: true,
-    });
-    // We're gonna change this later.
-    /*
-    fetch(REQUEST_URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(["firstOne", "secondOne", "thirdOne"],
-          loaded: true,
-        });
-      })
-      .done();
-    */
-  },
-
   render: function() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
-
     return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderItem}
-        style={styles.listView}
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'YelpScanner',
+          component: YelpScannerList,
+          passProps: {}
+        }}
+        itemWrapperStyle={styles.itemWrapper}
+        tintColor='#008888'
       />
-    );
-  },
-
-  renderLoadingView: function() {
-    return (
-      <View style={styles.container}>
-        <Text>
-          Loading results...
-        </Text>
-      </View>
-    );
-  },
-
-  renderItem: function(row) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>{row.name}</Text>
-      </View>
     );
   },
 });
@@ -86,30 +32,12 @@ var YelpScanner = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  rightContainer: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center',
-  },
-  year: {
-    textAlign: 'center',
-  },
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
+  itemWrapper: {
+    backgroundColor: '#eaeaea',
   },
 });
 
 AppRegistry.registerComponent('YelpScanner', () => YelpScanner);
+
+module.exports = YelpScanner;
